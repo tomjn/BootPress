c2d3a3c8cc87b3297352a4e0d49832a8291edc60
Tom Nowell <tarendai@darkstars.co.uk>
2012-02-11 00:57:37 +0000

    minor whitespace correction

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@ if ( ! function_exists( 'bootstrap_setup' ) ){
 				$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
 				$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
 				$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-				$attributes .= ($args->has_children) 				? ' class="dropdown-toggle"' 					   : ''; 
+				$attributes .= ($args->has_children) 		? ' class="dropdown-toggle"' 					   : ''; 
 
 				$item_output = $args->before;
 				$item_output .= '<a'. $attributes .'>';
